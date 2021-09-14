c62b305c03cd25ff36c19fbdf04ed52d27558529
Erlang Parasu <erlangparasu@gmail.com>
2021-09-14 12:14:57 +0800

    works

---
+++ example.1.php
@@ -0,0 +1,20 @@
+<?php
+
+exit; //
+
+// use ErlangParasu\PdfboxPhpWrapper\EpPdfboxPhpWrapper;
+
+// require __DIR__.'/vendor/autoload.php';
+
+// $file_pdf1 = '1.pdf';
+// $file_pdf2 = '2.pdf';
+// $file_pdf_output = __DIR__.'/output-example-1.pdf';
+
+// $pdfbox = new EpPdfboxPhpWrapper();
+// $pdfbox->addSourcePath($file_pdf1);
+// $pdfbox->addSourcePath($file_pdf2);
+// $pdfbox->setOutputPath($file_pdf_output);
+// $result = $pdfbox->merge();
+
+// var_dump([$result]);
+// exit;
--- src/EpPdfboxPhpWrapper.php
+++ src/EpPdfboxPhpWrapper.php
@@ -2,6 +2,111 @@
 
 namespace ErlangParasu\PdfboxPhpWrapper;
 
+use Exception;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
 class EpPdfboxPhpWrapper {
-    //
+
+    const JARFILE_PATH = __DIR__.'/../lib/pdfbox/pdfbox-app-2.0.24.jar';
+
+    public $source_paths = [];
+
+    public $output_path = null;
+
+    public function __constructor()
+    {
+        $this->source_paths = [];
+    }
+
+    public function resetSourcePaths()
+    {
+        $this->source_paths = [];
+    }
+
+    public function resetOutputPath()
+    {
+        $this->output_path = null;
+    }
+
+    public function addSourcePath($path)
+    {
+        if ($this->isPathValid($path)) {
+            $this->source_paths[] = $path;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function setOutputPath($path)
+    {
+        if (is_string($path)) {
+            if (strlen($path) > 1) {
+                $this->output_path = $path;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isPathValid($path) {
+        if (is_string($path)) {
+            if (file_exists($path)) {
+                if (is_file($path)) {
+                    if (is_readable($path)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function merge()
+    {
+        if (null == $this->output_path) {
+            return false;
+        }
+
+        if (0 == count($this->source_paths)) {
+            return false;
+        }
+
+        $words = [];
+        $words[] = 'java';
+        $words[] = '-jar';
+        $words[] = self::JARFILE_PATH;
+        $words[] = 'PDFMerger';
+
+        foreach ($this->source_paths as $i => $path) {
+            if ($this->isPathValid($path)) {
+                $words[] = $path;
+            } else {
+                throw new Exception('ERR_INVALID_SOURCE_PATH_'.$i);
+            }
+        }
+
+        $words[] = $this->output_path;
+
+        $process = new Process($words);
+        $process->run();
+
+        // executes after the command finishes
+        if (! $process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        // echo $process->getOutput();
+
+        if ($this->isPathValid($this->output_path)) {
+            return true;
+        }
+
+        return false;
+    }
 }
