2a68b2ef6d18b4850d790a93dac7b5f62b02a35d
Erlang Parasu <erlangparasu@gmail.com>
2021-12-01 12:29:25 +0800

    example

--- example-usage-3.php
+++ example-usage-3.php
@@ -1,16 +1,16 @@
 <?php
 
-exit; //
+exit;
 
-// use ErlangParasu\PdfboxPhpWrapper\EpPdfboxPhpWrapper;
+// use ErlangParasu\PdfboxPhpWrapper\EpPdfboxPhpWrapper3;
 
 // require __DIR__.'/vendor/autoload.php';
 
 // $file_pdf1 = '1.pdf';
 // $file_pdf2 = '2.pdf';
-// $file_pdf_output = __DIR__.'/output-example-1.pdf';
+// $file_pdf_output = __DIR__.'/output-example-3.pdf';
 
-// $pdfbox = new EpPdfboxPhpWrapper();
+// $pdfbox = new EpPdfboxPhpWrapper3();
 // $pdfbox->addSourcePath($file_pdf1);
 // $pdfbox->addSourcePath($file_pdf2);
 // $pdfbox->setOutputPath($file_pdf_output);
